53e08af6d84fe219b5c6f9365dae6861e310147e
Senthil Arumugam SP <senthil.aru@gmail.com>
2020-09-02 11:46:06 -0500

    intial commit-singleton

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Singleton
+{
+class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Singleton s1 = Singleton.GetInstance();
+              
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Singleton/Singleton/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton _instance ;
+
+        private Singleton()
+        {
+            Console.WriteLine("This is a Singleton pattern.");
+        }
+
+        public static Singleton GetInstance()
+        {
+            if(_instance == null)
+            {
+                 _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+    }
+}
